35d71bc276847aec6fd50e72c3f8559b3087138d
Muhammad Basyuny <mohamedfahmy407080@gmail.com>
2023-06-18 13:34:18 +0200

    Updating 1

--- S_NODE_02 Products categorization and currency conversions/node.js
+++ S_NODE_02 Products categorization and currency conversions/node.js
@@ -1,39 +1,55 @@
-import fetch from 'node-fetch'
 
 async function getProductsData () {
    const response = await fetch("https://api.escuelajs.co/api/v1/products");
    const data = await response.json();
-   console.log(data);
+   //console.log(data);
 
 
+   // List the Categories
    let categories = [];
-   for(let elem of categories){
-    
-   }
-
-   /*let map = new Map();
-
    for(let elem of data){
-    debugger;
-    //console.log(elem.category);
-    let arr = [];
-    let key = {};
-    let value = {};
-    const {id,title,price,description,image} = elem;
-    key = elem.category;
-    value ={id,title,price,description,image}; 
-    if(map.key){
-        arr = map.key;
-        arr.push(value);
-         map.key = arr;
+    
+    let obj = {
+        id: elem.category.id,
+        name: elem.category.name,
+        image: elem.category.image
     }
+    if(categories.find(categ => categ.id === obj.id));
     else{
-       map.key = arr.push(value);
+           categories.push(obj);
+   }
     }
     
-    
-   }
-  console.log(map);*/
+
+    //Sorting the Categories in Ascending Order
+    for (let i = 0; i < categories.length; ++i){
+        for(let j = 0; j < categories.length - 1; ++j){
+            if(categories[j].id > categories[j+1].id){
+                let temp = categories[j];
+                categories[j] = categories[j+1];
+                categories[j+1] = temp;
+            }
+        }
+    }
+  
+// Add Products to its own category
+  let newData = [];
+  for(let i = 0; i < categories.length; ++i ){
+    //debugger;
+    let category = categories[i];
+    let products = [];
+    for(let j = 0; j < data.length; ++j){
+       let obj = data[j];
+       const {id,title,description,image} = obj;
+       let product = {id,title,description,image};
+        if(obj.category.id === category.id){
+            products.push(product);
+           newData[i] = {category: category, products: products};
+        }
+
+    }
+  }
+  console.log(newData);
 
 }
 
