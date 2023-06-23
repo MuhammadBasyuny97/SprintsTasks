54efe2a85220cd8410d564c4b00defdf994f7a82
Muhammad Basyuny <mohamedfahmy407080@gmail.com>
2023-06-23 11:58:42 +0200

    Updating

--- S_Express_01 Write a CRUD for products/actions.js
+++ S_Express_01 Write a CRUD for products/actions.js
@@ -13,9 +13,10 @@ import { object, string, number} from 'yup';
 
 
 let products = [];
-
-export const getProducts =(req,res) => {
+ export const getProducts =(req,res) => {
+    const url = req.url;
     res.send(JSON.stringify(products));
+    res.end();
 }
 
 export const getProduct = (req,res) => {
@@ -23,8 +24,8 @@ export const getProduct = (req,res) => {
     let product = products.find(product => product.id === id);
 
     res.send(JSON.stringify(product));
+    res.end();
 }
-
 export const createProduct = (req,res) => {
     let body = req.body;
     let valid = userSchema.validateSync(body,{
@@ -45,12 +46,12 @@ export const createProduct = (req,res) => {
       else{
         res.send("Invalid Data");
       }
-
+      res.end();
 
 }
 
 
-export const updateProdcut =  (req,res) => {
+export const updateProduct =  (req,res) => {
     let id = req.params[0];
     let body = req.body;
     let valid = userSchema.validateSync(body,{
@@ -66,14 +67,24 @@ export const updateProdcut =  (req,res) => {
     else{
         res.send("Invalid Data");
     }
+    res.end();
    
 }
 
-
-export const deletProduct = (req,res) => {
+export const deleteProduct = (req,res) => {
     let id = req.params;
     let idx = products.findIndex(product => product.id === id);
     let product = products[idx];
     products.splice(idx,1);
     res.send(JSON.stringify(product));
+
+    res.end();
 }
+
+/* module.exports ={
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct
+} */
--- S_Express_01 Write a CRUD for products/index.js
+++ S_Express_01 Write a CRUD for products/index.js
@@ -1,19 +1,29 @@
-import { getProducts,getProduct,createProduct,updateProduct ,deleteProduct} from './actions';
+import { getProducts,getProduct,createProduct,updateProduct ,deleteProduct} from './actions.js';
+
+/* const getProducts = require('./actions.js');
+const getProduct =  require('./actions.js');
+const createProduct = require('./actions.js');
+const updateProduct = require('./actions.js');
+const deleteProduct = require('./actions.js'); */
+
+//const ctr = require('./actions');
 
 
 
-const express = require('express');
+
+import express from 'express';
+
 const app = express();
 
 
 app.get('/products', getProducts(req,res));
 
-app.get('/product/:id' , getProduct(req,res))
+app.get('/product/:id', getProduct(req,res))
 
 
-app.post('/products',createProduct(req,res))
-app.put('/product/:id/:price/:title' ,updateProduct(req,res))
-app.delete('/product/:id' , deleteProduct(req,res));
+app.post('/createProduct',createProduct(req,res))
+app.put('/updateProduct/:id' ,updateProduct(req,res))
+app.delete('/deleteProduct/:id' , deleteProduct(req,res));
 
 
 app.listen(3000, () => {
