668b044f07b4b5f660ec9a7ffdd660436683c079
MuhammadBasyuny97 <101678105+MuhammadBasyuny97@users.noreply.github.com>
2023-07-04 18:41:58 +0300

    Update app.js

--- S_Express_01 Write a CRUD for products/app.js
+++ S_Express_01 Write a CRUD for products/app.js
@@ -1,16 +1,5 @@
 import { getProducts,getProduct,createProduct,updateProduct ,deleteProduct} from './controllers/productContollers.js';
 
-/* const getProducts = require('./actions.js');
-const getProduct =  require('./actions.js');
-const createProduct = require('./actions.js');
-const updateProduct = require('./actions.js');
-const deleteProduct = require('./actions.js'); */
-
-//const ctr = require('./actions');
-
-
-
-
 import express from 'express';
 
 const app = express();
