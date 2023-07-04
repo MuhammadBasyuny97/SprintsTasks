cd31a7a47b9865b216e737e5b02dd7f4a4c77419
MuhammadBasyuny97 <101678105+MuhammadBasyuny97@users.noreply.github.com>
2023-07-05 02:00:57 +0300

    Update userAuthentication.js

--- S_Express_02 Project/middleware/userAuthentication.js
+++ S_Express_02 Project/middleware/userAuthentication.js
@@ -6,7 +6,6 @@ export const verifyToken =(req,res,next) => {
     console.log("Authorization", req.headers);
 
     let [_,token] = req.headers.authorization.split(" ");
-    //console.log("Authorization", req.headers);
     console.log("Token", token);
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     req.user = decoded;
